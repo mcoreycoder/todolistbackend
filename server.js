b1c3b7338f1524aca23291bdaf52a1543cbe654a
cdixon16-maker <schasedixon16@gmail.com>
2020-03-17 23:22:16 -0600

    my mistake

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/todos', todoRoutes);
 
-router,get('/', function(req, res) {
+router.get('/', function(req, res) {
     Todo.find(function(err, todos) {
         if (err) {
             console.log(err);
