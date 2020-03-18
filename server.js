efacda3b202ab93b989df415a33620918a971fe0
cdixon16-maker <schasedixon16@gmail.com>
2020-03-18 10:11:09 -0600

    asdf

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/todos', router);
 
-router.get('/todos', function(req, res) {
+router.get('/Todolist', function(req, res) {
     Todo.find(function(err, todos) {
         if (err) {
             console.log(err);
